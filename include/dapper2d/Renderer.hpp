3bf15d87075966d9b74c1a6989d9a95fbbffd424
DippityDapper <dustinchapline@gmail.com>
2025-11-04 12:35:14 -0500

    Added documentation and resource loader change

--- include/dapper2d/Renderer.hpp
+++ include/dapper2d/Renderer.hpp
@@ -10,20 +10,53 @@ namespace Engine
     class Sprite;
     class Camera;
 
+    /// A utility class that contains the renderer.
     class Renderer
     {
     private:
+        /// The SDL renderer object.
         static SDL_Renderer* renderer;
 
     public:
+        Renderer(const Renderer& other) = delete;
+
+        /// Initializes the renderer and ImGui. If the window is not initialized, this will fail.
+        /// @note This is called within the Engine.
         void Init();
+
+        /// Calls to present the current render.
+        /// @note This is called within the Engine.
         void Render();
+
+        /// Cleans the renderer and ImGui.
+        /// @note This is called within the Engine.
         void Clean();
 
+        /// Clears the buffer of the renderer and ImGui to be drawn into.
         static void BufferClear();
+
+        /// Adds a sprite's texture to the next draw pass.
+        /// @param position The world space position to draw the sprite.
+        /// @param sprite The sprite to draw the texture of.
+        /// @param camera The camera to use when calculating the screen position for the rendered texture.
+        /// If no camera is provided, the main camera will be used.
         static void BufferAdd(Vec2<float> position, Sprite* sprite, Camera* camera);
+
+        /// Adds a texture to the next draw pass.
+        /// @param position The world space position to draw the sprite.
+        /// @param texture The texture to draw.
+        /// @param camera The camera to use when calculating the screen position for the rendered texture.
+        /// If no camera is provided, the main camera will be used.
         static void BufferAdd(Vec2<float> position, SDL_Texture* texture, Camera* camera);
+
+        /// Adds a sprite's texture to the next draw pass.
+        /// @param position The screen space position to draw the sprite.
+        /// @param sprite The sprite to draw the texture of.
+        /// @note The camera is not used in the screen space calculation.
         static void BufferAdd(Vec2<float> position, Sprite* sprite);
+
+        /// Gets a pointer to the SDL renderer.
+        /// @returns A pointer to the SDL renderer.
         static SDL_Renderer* GetRenderer();
     };
 }
--- include/dapper2d/ResourceLoader.hpp
+++ include/dapper2d/ResourceLoader.hpp
@@ -9,8 +9,7 @@
 
 namespace Engine
 {
-
-
+    /// Used to pass into the shared pointer for proper cleanup of the texture.
     struct TextureDeleter
     {
         void operator()(SDL_Texture* texture) const noexcept
@@ -22,18 +21,48 @@ namespace Engine
         }
     };
 
+    /// Utility class that loads and creates textures.
+    ///
+    /// Unused textures are automatically cleaned periodically.
     class ResourceLoader
     {
     private:
+        /// A map of all loaded and created textures. A unique id is used to index into the map.
         static std::unordered_map<int, std::weak_ptr<SDL_Texture>> textures;
+
+        /// A map that tracks loaded texture file paths.
         static std::unordered_map<std::string, int> textureMap;
+
+        /// A utility map that converts a texture id to its file path.
         static std::unordered_map<int, std::string> idToPath;
+
+        /// The scale mode to use when loading or creating textures scale, ie nearest, linear, or pixel art
         static SDL_ScaleMode scaleMode;
 
     public:
-        static std::shared_ptr<SDL_Texture> LoadTexture(const std::string& texturePath);
+        ResourceLoader(const ResourceLoader& other) = delete;
+
+        /// Loads a texture given a file path.
+        /// If the file path is found in the texture map, that texture will be returned.
+        /// @param texturePath The file path of the texture.
+        /// @returns a shared pointer to the SDL texture.
+        static std::shared_ptr<SDL_Texture> LoadTexture(const std::string& filePath);
+
+        /// Creates a texture.
+        /// @param format The pixel format for the texture.
+        /// @param access The texture access for the texture.
+        /// @param w The width of the texture.
+        /// @param h The height of the texture.
+        /// @returns a shared pointer to the SDL texture.
         static std::shared_ptr<SDL_Texture> CreateTexture(SDL_PixelFormat format, SDL_TextureAccess access, int w, int h);
+
+        /// Cleans unused textures.
+        /// @param maxPerCall The max amount of textures that are cleaned per call.
+        /// @note This is called within the Engine update loop.
         static void CleanExpired(size_t maxPerCall = 10);
+
+        /// Sets the scale mode of loaded textures.
+        /// @param _scaleMode The SDL scale mode.
         static void SetScaleMode(SDL_ScaleMode _scaleMode);
     };
 }
